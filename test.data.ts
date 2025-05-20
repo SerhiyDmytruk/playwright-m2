ac8f48833d84258c6bf2f2478cf3c5d3eac4b442
Serhiy Dmytruk <dmitruksl92@gmail.com>
2025-05-20 09:21:57 +0300

    Update test.data.ts

--- test.data.ts
+++ test.data.ts
@@ -3,6 +3,6 @@ import {configs} from "./configs";
 export const testData = {
     URL : configs.URL,
     pathAccount : configs.pathAccount,
-    email : 'ernst@hoofdfabriek.nl',
-    password : 'S321654s'
+    email : '',
+    password : ''
 }
